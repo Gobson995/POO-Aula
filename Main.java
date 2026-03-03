849f496c2e2fa6fffe3d539ac3ee748c70a60077
Gobson995 <gugabadazanatta@gmail.com>
2026-03-03 14:55:07 -0300

    primeiro codigo e baixando o necessario

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main (String[] args) {
+        String mensagem = "Opa, estou testando o java";
+
+        System.out.println(mensagem);
+
+        imprimirDespedida();
+    }
+
+    public static void imprimirDespedida() {
+        System.out.println("Execução finalizada com sucesso");
+    }
+}
